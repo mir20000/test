511f7c689662337f77c94767a995d896e05240f0
mir20000 <57058150+mir20000@users.noreply.github.com>
2020-07-27 14:06:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def home(latitude, longitude):
+    return '<h1>you dont have permission to use this app without any value</h1>'
+
+
 @app.route('/formapp/<latitude>/<longitude>')
-def index(latitude,longitude):
-    return '<h1>your input<br><br> latitude=> {} <br> longitude=> {}</h1>'.format(latitude,longitude)
+def index(latitude, longitude):
+
+    geocode = [latitude, longitude]
+
+    return render_template('getloc.html', geocode=geocode)
